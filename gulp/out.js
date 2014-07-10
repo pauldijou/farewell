4893d1ff91c2fc5e9eceb4afbecbb6c94c752bb3
Paul Dijou <paul.dijou@gmail.com>
2014-07-10 23:55:03 +1200

    Chain tasks

--- gulp/out.js
+++ gulp/out.js
@@ -3,29 +3,30 @@ var ignore       = require('gulp-ignore');
 var gulp         = require('gulp');
 var handleErrors = require('./utils/handleErrors');
 
-gulp.task('out:clean', function() {
+// For now, ugly hack chaining tasks
+gulp.task('out:clean', ['build'], function() {
   return gulp.src(['./out/img', './out/resources', './out/*.*', './out/CNAME'], {read: false})
     .pipe(ignore('./out/.git'))
     .pipe(rimraf())
     .on('error', handleErrors);
 });
 
-gulp.task('out:root', function() {
+gulp.task('out:root', ['out:clean'], function() {
   return gulp.src(['./CNAME', './*.ico'])
     .pipe(gulp.dest('./out'))
     .on('error', handleErrors);
 });
 
-gulp.task('out:img', function() {
+gulp.task('out:img', ['out:root'], function() {
   return gulp.src(['./img/*'])
     .pipe(gulp.dest('./out/img'))
     .on('error', handleErrors);
 });
 
-gulp.task('out:fonts', function() {
+gulp.task('out:fonts', ['out:img'], function() {
   return gulp.src(['./resources/fonts/**/*'])
     .pipe(gulp.dest('./out/resources/fonts'))
     .on('error', handleErrors);
 });
 
-gulp.task('out', ['out:clean', 'out:root', 'out:img', 'out:fonts', 'build']);
+gulp.task('out', ['out:fonts']);
